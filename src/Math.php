54f1fb4cc4b8829dc205608c24660782e2b24f9a
Daniel Burgner <daniel.burgner@dominionenterprises.com>
2015-06-15 13:02:14 -0400

    Added multiply function

--- src/Math.php
+++ src/Math.php
@@ -4,4 +4,11 @@ namespace Chadicus;
 
 abstract class Math
 {
+    public static function multiply($a, $b)
+    {
+        if (!is_numeric($a) || !is_numeric($b)) {
+            throw new \InvalidArgumentException('Both inputs must be numeric');
+        }
+        return ($a * $b);
+    }
 }
--- tests/MathTest.php
+++ tests/MathTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace ChadicusTest;
 
+use Chadicus\Math;
+
 /**
  * Unit tests for the \Chadicus\Math class
  *
@@ -8,4 +10,29 @@ namespace ChadicusTest;
  */
 final class MathTest extends \PHPUnit_Framework_TestCase
 {
+	/**
+	 * Verify basic behavior of multiply method
+	 * 
+	 * @test
+	 * @covers ::multiply
+	 * 
+	 * return void
+	 */
+	public function multiply() {
+		$this->assertSame(25, Math::multiply(5, 5), 'Math::multiply() did not return 25 for 5 * 5');
+	}
+	
+	/**
+	 * Verify behavior of multiply when one argument is not a number
+	 * 
+	 * @test
+	 * @covers ::multiply
+	 * @expectedException \InvalidArgumentException
+	 * @expectedExceptionMessage Both inputs must be numeric
+	 * 
+	 * return void
+	 */
+	public function multiply_nonNumericInput() {
+		Math::multiply('a', 5);
+	}
 }
