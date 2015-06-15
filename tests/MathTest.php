2837833d5fe1b3f9243299c48ac0f8946453af35
Chad Gray <chadwickgray@gmail.com>
2015-06-15 12:15:11 -0400

    Add empty test and dev autoload

---
+++ tests/MathTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace ChadicusTest;
+
+/**
+ * Unit tests for the \Chadicus\Math class
+ *
+ * @coversDefaultClass \Chadicus\Math
+ */
+final class MathTest extends \PHPUnit_Framework_TestCase
+{
+}
