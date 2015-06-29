0645ac7cb09f11fcad3634c18d728e49cff24023
Chad Gray <chadwickgray@gmail.com>
2015-06-29 11:21:44 -0400

    Add empty test for HelloWorld class

---
+++ tests/HelloWorldTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace ChadicusTest;
+
+use Chadicus\HelloWorld;
+
+/**
+ * Unit tests for the \Chadicus\HelloWorld class
+ *
+ * @coversDefaultClass \Chadicus\HelloWorld
+ */
+final class HelloWorldTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Verify basic behavior of greet()
+     *
+     * @test
+     * @covers ::greet
+     *
+     * return void
+     */
+    public function greet()
+    {
+        //@TODO imlement test
+    }
+}
